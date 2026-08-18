d83a3138965dae337c5d73f0cec5c693767cd5b0
Your Name <kshitijkhandelwal112@gmail.com>
2026-08-18 21:48:17 +0530

    adding cleaner solution given by AI

--- Codeforces/800/86-Mehmoud_and_Ehab_and_Even-Odd_Game.cpp
+++ Codeforces/800/86-Mehmoud_and_Ehab_and_Even-Odd_Game.cpp
@@ -10,3 +10,12 @@ int main(){
     }
     return 0;
 }
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << (n % 2 == 0 ? "Mahmoud" : "Ehab");
+} */
