b00c17bdf5ef292e527af1998245b18fe2564309
Your Name <kshitijkhandelwal112@gmail.com>
2026-08-19 18:56:13 +0530

    new solution

---
+++ Codeforces/800/92-Yogurt_Sale.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,a,b;
+        cin>>n>>a>>b;
+        if(b>=(2*a)) cout<<n*a<<endl;
+        else{
+            cout<<(n/2)*b + (n%2)*a<<endl;
+        }
+    }
+    return 0;
+}
